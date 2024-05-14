a312f0523c2e6d3914192445b56b6b398573745c
Sozan-hama-kaka <sozan.hama-kaka@s2020.tu-chemnitz.de>
2024-05-14 20:23:28 +0200

    Creating views done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\ExampleController::class, "homepage"]);
+Route::get('/about', [\App\Http\Controllers\ExampleController::class, "aboutPage"]);
